67eefb98ae60404da00efa47b25ce823d1c9e294
Alex Garuba <alexandergaruba@gmail.com>
2022-06-20 22:02:06 -0400

    fix: footer styling and Navigation links

--- components/layout/footer.jsx
+++ components/layout/footer.jsx
@@ -6,12 +6,12 @@ import SocialIconBtn from "../buttons/social-icon-btn";
 
 function Footer() {
   return (
-    <div className="max-w-7xl mx-auto px-4 sm:px-6 z-[10]">
+    <div className="max-w-[1440px] mx-auto px-4 sm:px-6 z-[10]">
       <footer className="footer"></footer>
 
       <div className="footer">
-        <div className="content">
-          <div className="md:flex md:px-20">
+        <div className="content md:px-20">
+          <div className="md:flex">
             <div className="w-full md:w-1/3">
               <div className="flex items-center">
                 <a className="w-12 md:w-20" href="/" >
@@ -22,9 +22,8 @@ function Footer() {
                       width="350px"
                     />
                 </a>
-                <span className="content-title">THE GARDEN</span>
               </div>
-              <div className="mt-8 md:mt-12 text-2xs md:text-sm">Connect with us</div>
+              <div className="mt-8 text-2xs md:text-sm">Connect with us</div>
               {/* Socials */}
               <div className="flex gap-7 mb-5">
                 <SocialIconBtn
@@ -41,17 +40,19 @@ function Footer() {
                 />
               </div>
             </div>
-            <div className="md:w-2/3">
+            <div className="md:w-2/3 md:flex md:justify-end">
               <div className="mt-8 text-sm md:flex md:mt-0">
                 <div className="flex-1">
-                  <div><a href="/gallery">Gallery</a></div>
-                  <div className="footer-menu"><a href="/for-sharing">For Sharing</a></div>
-                  <div className="footer-menu"><a href="/for-supporter">For Supporters</a></div>
+                  <div className="footer-menu"><a href="/">Home</a></div>
+                  <div className="footer-menu"><a href="/">Gallery</a></div>
+                  <div className="footer-menu"><a href="/">Store</a></div>
+                  <div className="footer-menu"><a href="/">About</a></div>
+                  <div className="footer-menu"><a href="/">Journey</a></div>
                 </div>
               </div>
             </div>
           </div>
-          <div className="content-copyright">© 2022 Coloma Labs</div>
+          <div className="mt-4">© 2022 Coloma Labs</div>
         </div>
       </div>
 
