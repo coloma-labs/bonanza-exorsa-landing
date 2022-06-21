2b7e4764e8a7aae33d65aa4319bef95b526e3b1f
Alex Garuba <alexandergaruba@gmail.com>
2022-06-21 00:14:24 -0400

    copy: add home page text and styling

--- pages/index.jsx
+++ pages/index.jsx
@@ -72,6 +72,46 @@ const Home = () => {
           </div>
         </div> */}
 
+        <section>
+          <div className="section-title">
+            a metaphysical brand.
+          </div>
+          <div className="section-content">
+            Bonanza, is the world’s first ever Metaphysical Brand. Our brand<br />symbolizes one’s journey towards the investigation of reality,
+            self-worth,<br />identity, and possibility.
+          </div>
+        </section>
+
+        <section>
+          <div className="section-title text-right">
+            the bonan collection.
+          </div>
+          <div className="section-content text-right">
+            The Bonan Collection contains 4000 different variations of Bonan, each<br />which have its own unique personality and swagger.<br /><br />
+            
+            The story of Bonan was conceptualized and written by the Founder of,<br />Bonanza, Johnathon Tesfaye, and is based on real-life events derived<br />
+            from his own life. Each variation of Bonan was brought to life by the<br />work of Ian Lawrence, the Creative Director of Bonanza.<br /><br />
+            
+            Ian and Johnathon meticulously designed characteristics of this<br />collection to their exact specifications. Aggregating interests from<br />
+            traditional Japanese anime, to urban fashion and hip-hop culture into<br />one overarching brand.
+          </div>
+        </section>
+
+        <section>
+          <div className="section-title">
+            synchronizing value.
+          </div>
+          <div className="section-content">
+          The Bonanza team aims to garner a community of young entrepreneurs,<br />creators, and artists that can express themselves through our unique<br />brand.<br /><br /> 
+
+          Bonanza NFTs demonstrate your individual creativity, adventure, and<br />individuality. Additionally, Bonanza NFTs plan to provide utility allowing<br />
+          holders to gain exclusive access to IRL events, networks,<br />merchandising, and more.<br /><br />
+
+          We believe that by educating our community on the value of our NFT<br />propositions, we can build an agile ecosystem moving forward with<br />unlimited
+          possibilities.  
+          </div>
+        </section>
+
         <Footer />
 
       </div>
