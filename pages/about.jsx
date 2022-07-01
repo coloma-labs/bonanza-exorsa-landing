e0bb6170c80adc33cc265ade7f3dc4537c682026
Alex Garuba <alexandergaruba@gmail.com>
2022-06-30 23:15:27 -0400

    copy: update about page

--- pages/about.jsx
+++ pages/about.jsx
@@ -37,7 +37,7 @@ const About = () => {
           id="about-banner"
           className="min-h-[120px] md:min-h-[150px] lg:min-h-[180px] animate-banner-load px-5 flex items-center mb-20"
         >
-          <h1 className="text-6xl md:text-7xl lg:text-8xl text-white font-bold animate-fade-in">
+          <h1 className="text-6xl md:text-7xl lg:text-8xl text-white animate-fade-in">
             About
           </h1>
         </div>
@@ -46,54 +46,49 @@ const About = () => {
           <div className="max-w-[600px]">
             <div className="section-title">a metaphysical brand.</div>
             <div className="section-content">
-              Bonanza, is the world’s first ever Metaphysical Brand. Our brand
-              symbolizes one’s journey towards the investigation of reality,
-              self-worth, identity, and possibility.
+              Bonanza is the world’s first ever Metaphysical Brand. Our
+              team aims to garner a community of young entrepreneurs,
+              creators, and artists that can utilize our digital and physical
+              asset offerings to express themselves.
             </div>
           </div>
 
           <div className="flex justify-end">
             <div className="max-w-[600px]">
               <div className="section-title text-right mt-10">
-                the bonan collection.
+                our value proposition.
               </div>
               <div className="section-content text-right">
-                The Bonan Collection contains 4000 different variations of
-                Bonan, each which have its own unique personality and swagger.
+                A Bonanza NFT demonstrates your individual creativity, and
+                individuality. Additionally, Bonanza NFTs serve as lifelong
+                assets that provide IRL utility. Allowing holders to meet one
+                another at our global events. 
                 <br />
                 <br />
-                The story of Bonan was conceptualized and written by the
-                Founder of, Bonanza, Johnathon Tesfaye, and is based on
-                real-life events derived from his own life. Each variation of
-                Bonan was brought to life by the work of Ian Lawrence, the
-                Creative Director of Bonanza.
-                <br />
-                <br />
-                Ian and Johnathon meticulously designed characteristics of
-                this collection to their exact specifications. Aggregating
-                interests from traditional Japanese anime, to urban fashion
-                and hip-hop culture into one overarching brand.
+                We believe that by educating our community on the value of
+                our NFT propositions, we can build an agile ecosystem with
+                unlimited possibilities for future collections.
               </div>
             </div>
           </div>
 
           <div className="max-w-[600px]">
-            <div className="section-title mt-10">synchronizing value.</div>
+            <div className="section-title mt-10">the bonan collection.</div>
             <div className="section-content">
-              The Bonanza team aims to garner a community of young
-              entrepreneurs, creators, and artists that can express themselves
-              through our unique brand.
+              The Bonan Collection contains 4000 different variations of Bonan, 
+              each which have their own unique personality and swagger.
               <br />
               <br />
-              Bonanza NFTs demonstrate your individual creativity, adventure,
-              and individuality. Additionally, Bonanza NFTs plan to provide
-              utility allowing holders to gain exclusive access to IRL events,
-              networks, merchandising, and more.
+              The story of Bonan was conceptualized and written by the 
+              Founder of Bonanza, Johnathon Tesfaye, and is based on a story of his
+              own life. Each variation of Bonan was then brought to life by the work
+              of Ian Lawrence, the Creative Director of Bonanza.  
               <br />
               <br />
-              We believe that by educating our community on the value of our
-              NFT propositions, we can build an agile ecosystem moving forward
-              with unlimited possibilities.
+              Ian and Johnathon meticulously designed the characteristics of this
+              collection to their exact specifications. Aggregating interests from
+              traditional Japanese anime, to urban fashion and hip-hop culture into
+              one overarching brand.
             </div>
           </div>
         </section>
