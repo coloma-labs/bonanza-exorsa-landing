e597e5631109db84bd6aa08bdb4bca5a6fdc6b3a
Alex Garuba <alexandergaruba@gmail.com>
2022-06-30 22:30:07 -0400

    fix: add layout to homepage

--- pages/index.jsx
+++ pages/index.jsx
@@ -12,6 +12,7 @@ import Image from "next/image";
 import { FaTwitter, FaDiscord } from "react-icons/fa";
 import { RiInstagramFill } from "react-icons/ri";
 import { useWindowSize } from "../hooks/window";
+import Layout from "../components/layout";
 
 const Home = () => {
   const [manifestoIsOpen, setManifestoIsOpen] = useState(false);
@@ -66,105 +67,106 @@ const Home = () => {
           content="nft, opensea, nft meaning, nft art, nft crypto, non fungible token, opensea nft, nft token, nft crypto art, crypto nft, coinbase nft, nfts crypto, nft buy, nft for sale, buy nfts, nft opensea, crypto, cryptocurrency, ethereum, eth"
         />
       </Head>
-
-      <div
-        id="home-page"
-        className={`${
-          manifestoIsOpen ? "overflow-y-hidden" : "overflow-y-auto"
-        }`}
-      >
-        <NavBar />
-
-        {/* Background Video */}
-        <video className="absolute -z-10" autoPlay muted loop id="bgVideo">
-          <source src={backgroundVideo} type="video/mp4" />
-        </video>
-
-        {/* <div
-          className={`flex justify-center items-center manifesto-wrapper transition duration-500 
-          ${scrollY > 500 ? "bg-[#f3f5f4]" : "bg-transparent"}
-          ${manifestoIsOpen ? "!absolute w-full top-[-2px]" : ""}
-        `}
-        >
-          <Manifesto
-            setIsOpen={setManifestoIsOpen}
-            isOpen={manifestoIsOpen}
-            isScrolled={scrollY > 500}
-          />
-        </div> */}
-
+      
+      <Layout>
         <div
-          className={'absolute bottom-0 left-0 h-screen w-full flex flex-col justify-center items-center'}
+          id="home-page"
+          className={`${
+            manifestoIsOpen ? "overflow-y-hidden" : "overflow-y-auto"
+          }`}
         >
-          <div className="pb-44" />
-          <Image
-            src="/logos/bonanza-long.png"
-            alt="Bonanza"
-            height="90px"
-            width="250px"
-          />
-        </div>
 
-        {/* <div className="bg-[#f3f5f4]">
-          <section className="bg-[#f3f5f4] py-20 lg:py-50 2xl:py-[10rem] px-5">
-            <div className="max-w-[600px]">
-              <div className="section-title">a metaphysical brand.</div>
-              <div className="section-content">
-                Bonanza, is the world’s first ever Metaphysical Brand. Our brand
-                symbolizes one’s journey towards the investigation of reality,
-                self-worth, identity, and possibility.
-              </div>
-            </div>
+          {/* Background Video */}
+          <video className="absolute -z-10" autoPlay muted loop id="bgVideo">
+            <source src={backgroundVideo} type="video/mp4" />
+          </video>
 
-            <div className="flex justify-end">
+          {/* <div
+            className={`flex justify-center items-center manifesto-wrapper transition duration-500 
+            ${scrollY > 500 ? "bg-[#f3f5f4]" : "bg-transparent"}
+            ${manifestoIsOpen ? "!absolute w-full top-[-2px]" : ""}
+          `}
+          >
+            <Manifesto
+              setIsOpen={setManifestoIsOpen}
+              isOpen={manifestoIsOpen}
+              isScrolled={scrollY > 500}
+            />
+          </div> */}
+
+          <div
+            className={'absolute bottom-0 left-0 h-screen w-full flex flex-col justify-center items-center'}
+          >
+            <div className="pb-44" />
+            <Image
+              src="/logos/bonanza-long.png"
+              alt="Bonanza"
+              height="90px"
+              width="250px"
+            />
+          </div>
+
+          {/* <div className="bg-[#f3f5f4]">
+            <section className="bg-[#f3f5f4] py-20 lg:py-50 2xl:py-[10rem] px-5">
               <div className="max-w-[600px]">
-                <div className="section-title text-right mt-10">
-                  the bonan collection.
-                </div>
-                <div className="section-content text-right">
-                  The Bonan Collection contains 4000 different variations of
-                  Bonan, each which have its own unique personality and swagger.
-                  <br />
-                  <br />
-                  The story of Bonan was conceptualized and written by the
-                  Founder of, Bonanza, Johnathon Tesfaye, and is based on
-                  real-life events derived from his own life. Each variation of
-                  Bonan was brought to life by the work of Ian Lawrence, the
-                  Creative Director of Bonanza.
-                  <br />
-                  <br />
-                  Ian and Johnathon meticulously designed characteristics of
-                  this collection to their exact specifications. Aggregating
-                  interests from traditional Japanese anime, to urban fashion
-                  and hip-hop culture into one overarching brand.
+                <div className="section-title">a metaphysical brand.</div>
+                <div className="section-content">
+                  Bonanza, is the world’s first ever Metaphysical Brand. Our brand
+                  symbolizes one’s journey towards the investigation of reality,
+                  self-worth, identity, and possibility.
                 </div>
               </div>
-            </div>
 
-            <div className="max-w-[600px]">
-              <div className="section-title mt-10">synchronizing value.</div>
-              <div className="section-content">
-                The Bonanza team aims to garner a community of young
-                entrepreneurs, creators, and artists that can express themselves
-                through our unique brand.
-                <br />
-                <br />
-                Bonanza NFTs demonstrate your individual creativity, adventure,
-                and individuality. Additionally, Bonanza NFTs plan to provide
-                utility allowing holders to gain exclusive access to IRL events,
-                networks, merchandising, and more.
-                <br />
-                <br />
-                We believe that by educating our community on the value of our
-                NFT propositions, we can build an agile ecosystem moving forward
-                with unlimited possibilities.
+              <div className="flex justify-end">
+                <div className="max-w-[600px]">
+                  <div className="section-title text-right mt-10">
+                    the bonan collection.
+                  </div>
+                  <div className="section-content text-right">
+                    The Bonan Collection contains 4000 different variations of
+                    Bonan, each which have its own unique personality and swagger.
+                    <br />
+                    <br />
+                    The story of Bonan was conceptualized and written by the
+                    Founder of, Bonanza, Johnathon Tesfaye, and is based on
+                    real-life events derived from his own life. Each variation of
+                    Bonan was brought to life by the work of Ian Lawrence, the
+                    Creative Director of Bonanza.
+                    <br />
+                    <br />
+                    Ian and Johnathon meticulously designed characteristics of
+                    this collection to their exact specifications. Aggregating
+                    interests from traditional Japanese anime, to urban fashion
+                    and hip-hop culture into one overarching brand.
+                  </div>
+                </div>
               </div>
-            </div>
-          </section>
-        </div> */}
 
-        {/* <Footer /> */}
-      </div>
+              <div className="max-w-[600px]">
+                <div className="section-title mt-10">synchronizing value.</div>
+                <div className="section-content">
+                  The Bonanza team aims to garner a community of young
+                  entrepreneurs, creators, and artists that can express themselves
+                  through our unique brand.
+                  <br />
+                  <br />
+                  Bonanza NFTs demonstrate your individual creativity, adventure,
+                  and individuality. Additionally, Bonanza NFTs plan to provide
+                  utility allowing holders to gain exclusive access to IRL events,
+                  networks, merchandising, and more.
+                  <br />
+                  <br />
+                  We believe that by educating our community on the value of our
+                  NFT propositions, we can build an agile ecosystem moving forward
+                  with unlimited possibilities.
+                </div>
+              </div>
+            </section>
+          </div> */}
+
+          {/* <Footer /> */}
+        </div>
+      </Layout>
     </>
   );
 };
