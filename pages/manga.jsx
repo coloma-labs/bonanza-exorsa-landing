f168998b7388f40dffdea89bc22fee27217b92da
Alex Garuba <alexandergaruba@gmail.com>
2022-07-23 22:50:59 -0400

    feat: CSS flipbook added to manga page desktop only

--- pages/manga.jsx
+++ pages/manga.jsx
@@ -1,28 +1,14 @@
 // Components
 import Head from "next/head";
 import Image from "next/image";
-import Script from "next/script";
-import { useEffect, useState } from "react";
 import Layout from "../components/layout";
+import { useWindowSize } from "../hooks/window";
 
 const Manga = () => {
-  return (
+  const mobile = useWindowSize().width < 768;
+  return mobile ? (
     <>
-      <Script
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=G-0DQTSRH3RD`}
-      />
-
-      <Script strategy="lazyOnload">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-0DQTSRH3RD', {
-            page_path: window.location.pathname,
-          });
-        `}
-      </Script>
+      {/* MOBILE */}
 
       <Head>
         <title>Bonanza Exorsa - Manga</title>
@@ -46,12 +32,137 @@ const Manga = () => {
                   alt={`Manga ${el}`}
                   width="2867"
                   height="4096"
-                  loading="lazy"
                 />
               ))}
             </div>
           </div>
         </div>
+      </Layout>
+    </>
+  ) : (
+    <>
+      {/* DESKTOP */}
+      {/* <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-0DQTSRH3RD`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-0DQTSRH3RD', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script> */}
+
+      <Head>
+        <title>Bonanza Exorsa - Manga</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="keywords"
+          content="nft, opensea, nft meaning, nft art, nft crypto, non fungible token, opensea nft, nft token, nft crypto art, crypto nft, coinbase nft, nfts crypto, nft buy, nft for sale, buy nfts, nft opensea, crypto, cryptocurrency, ethereum, eth"
+        />
+      </Head>
+
+      <Layout>
+        
+        {/* CSS flipbook
+              -inside cover pages at the front and back of book
+              -divided into physical pages which have a front and back
+
+              TODO: Front cover with logo and book opening animation on page load
+              TODO: Change from page flip buttons to clicking anywhere on the
+                    page will flip in that direction
+         */}
+
+        <div className="book">
+          <input type="checkbox" id="c1"/>
+          <input type="checkbox" id="c2"/>
+          <input type="checkbox" id="c3"/>
+
+          <div id="cover" />
+
+          <div className="flip-book">
+
+            {/* First physical page */}
+
+            <div className="flip" id="p1">
+              <div className="front">
+                <Image
+                  src={`/manga/BONAN_MANGA_01.svg`}
+                  alt={`Manga 1`}
+                  width="2867"
+                  height="4096"
+                />
+                <label className="flip-btn" for="c1">Next</label>
+              </div>
+
+              <div className="back">
+                <Image
+                  src={`/manga/BONAN_MANGA_02.svg`}
+                  alt={`Manga 2`}
+                  width="2867"
+                  height="4096"
+                />
+                <label className="flip-btn" for="c1">Back</label>
+              </div>
+
+            </div>
+
+            {/* Second physical page */}
+
+            <div className="flip" id="p2">
+              <div className="front">
+                <Image
+                  src={`/manga/BONAN_MANGA_03.svg`}
+                  alt={`Manga 3`}
+                  width="2867"
+                  height="4096"
+                />
+                <label className="flip-btn" for="c2">Next</label>
+              </div>
+              <div className="back">
+                <Image
+                  src={`/manga/BONAN_MANGA_05.svg`}
+                  alt={`Manga 5`}
+                  width="2867"
+                  height="4096"
+                />
+                <label className="flip-btn" for="c2">Back</label>
+              </div>
+            </div>
+
+            {/* Third physical page */}
+
+            <div className="flip" id="p3">
+              <div className="front">
+                <Image
+                  src={`/manga/BONAN_MANGA_06.svg`}
+                  alt={`Manga 6`}
+                  width="2867"
+                  height="4096"
+                />
+                <label className="flip-btn" for="c3">Next</label>
+              </div>
+              <div className="back">
+                <Image
+                  src={`/manga/BONAN_MANGA_07.svg`}
+                  alt={`Manga 7`}
+                  width="2867"
+                  height="4096"
+                />
+                <label className="flip-btn" for="c3">Back</label>
+              </div>
+            </div>
+
+            <div id="cover" />
+
+          </div>
+        </div>
+
       </Layout>
     </>
   );
