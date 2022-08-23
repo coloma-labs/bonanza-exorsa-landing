4cc4724d832fee3e36967c199a59e9b0557a0c03
Alex Garuba <alexandergaruba@gmail.com>
2022-08-22 20:23:50 -0400

    fix: remove soundcloud player on mobile

--- pages/_app.js
+++ pages/_app.js
@@ -22,7 +22,7 @@ function App({ Component, pageProps }) {
 
     {/* Soundcloud Player */}
     <div
-      className='fixed bottom-0 right-0 overflow-hidden animate-fade-in animation-delay-1000 z-10'>
+      className='hidden md:block fixed bottom-0 right-0 overflow-hidden animate-fade-in animation-delay-1000 z-10'>
       <iframe
         width="100%" height="90"
         scrolling="no"
