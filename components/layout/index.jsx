5c1aff1a94e70bf837a122e4318209b2116054ee
yoyobojo <angellolazar@gmail.com>
2022-11-22 21:59:04 -0500

    removed navbar

--- components/layout/index.jsx
+++ components/layout/index.jsx
@@ -5,7 +5,7 @@ import Navbar from "./navbar";
 function Layout({ children, style }) {
   return (
     <div className={style}>
-      <Navbar />
+      {/* <Navbar /> */}
       <div className="max-w-6xl mx-auto p-5 mb-10 lg:mb-20">{children}</div>
       <Footer />
     </div>
